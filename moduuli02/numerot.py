6d72702d27a844dd79811f79647d0355b89ec8df
Jafestro <jafestro@gmail.com>
2023-08-22 12:43:00 +0300

    tehtavat 2

--- moduuli02/numerot.py
+++ moduuli02/numerot.py
@@ -1,4 +1,8 @@
 import random
 
+
+# kolmenumeroisen koodin, jonka kukin numeromerkki on väliltä 0..9.
 print(f"{random.randint(0, 9)}{random.randint(0, 9)}{random.randint(0, 9)}")
+print()
+# nelinumeroisen koodin, jonka kukin numeromerkki on väliltä 1..6.
 print(f"{random.randint(0, 6)}{random.randint(0, 6)}{random.randint(0, 6)}{random.randint(0, 6)}")
