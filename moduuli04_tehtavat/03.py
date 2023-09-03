77d841aedd4aaf814e2b878311b30ef17782807a
Jafestro <jafestro@gmail.com>
2023-09-03 14:16:49 +0300

    optimized

--- moduuli04_tehtavat/03.py
+++ moduuli04_tehtavat/03.py
@@ -1,12 +1,14 @@
 user = 0
-list = list()
+lista = list()
 
 while user != "":
 
-    user = input("Syötä numeroita siis ohjelma tulostaa saaduista luvuista pienimmän ja suurimman: (Jos halut pois syötä tyhjän merkkijonon)\n")
+    user = input(
+        "Syötä numeroita siis ohjelma tulostaa saaduista luvuista pienimmän "
+        "ja suurimman: (Jos halut pois syötä tyhjän merkkijonon)\n")
 
     if user.isdigit() or user[1:].isdigit():
-        list.append(int(user))
+        lista.append(int(user))
 
-print(f"Pienin arvo listassa on {min(list)}")
-print(f"Suurin arvo listassa on {max(list)}")
+print(f"Pienin arvo listassa on {min(lista)}")
+print(f"Suurin arvo listassa on {max(lista)}")
