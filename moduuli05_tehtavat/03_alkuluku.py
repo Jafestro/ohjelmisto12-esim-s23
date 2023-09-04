136d7341c13fa637c7666fcf821741acc745eae0
Jafestro <jafestro@gmail.com>
2023-09-04 14:50:05 +0300

    optimized

--- moduuli05_tehtavat/03_alkuluku.py
+++ moduuli05_tehtavat/03_alkuluku.py
@@ -5,11 +5,9 @@ if luku <= 1:
 else:
     alku = True
     for i in range(2, int(luku ** 0.5) + 1):
-        if luku % i == 0:
-            if i != luku:
-                print(f"{luku} ei oo alkuluku")
-                alku = False
-                break
+        if luku % i == 0 and luku != i or luku == 2:
+            print(f"{luku} ei oo alkuluku")
+            alku = False
+            break
     if alku:
         print(f"{luku} on alkuluku.")
-
