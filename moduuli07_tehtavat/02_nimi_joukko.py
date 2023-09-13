c8857e01ab0c5e8aa840fe4af3adca53c18ef128
Jafestro <jafestro@gmail.com>
2023-09-13 21:47:41 +0300

    lisäsin komenteja

--- moduuli07_tehtavat/02_nimi_joukko.py
+++ moduuli07_tehtavat/02_nimi_joukko.py
@@ -1,7 +1,9 @@
 nimet = set()
 
+# Käyttäjä antaa nimet ja jos annettu nimi on uusi se lisätty joukon
+# mutta jos se on jo aiemmin syötetty nimi siis ei lisätään joukoon
 while True:
-    nimi = str(input("Syötä nimesi: "))
+    nimi = str(input("Syötä nimesi: (Jos halut pois paina vain Enter) \n"))
     if nimi == "":
         print("Nähään!")
         break
@@ -10,6 +12,7 @@ while True:
     elif nimi not in nimet:
         print("Uusi nimi:")
         nimet.add(nimi)
+# Tulostaa kaikki nimet, että ovat joukossa
 print("Kaikki nimet joukossa: ")
 for name in nimet:
     print(name)
